75fe4f91779563c34e3f3b0fdeb49a45ae3379fa
olawanlejoeldanielafriheart <esuoladaniel002@gmail.com>
2024-02-29 11:53:06 +0100

    Added Versel.Json file

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -9,7 +9,7 @@ const Gallery = ({ Src }) => {
     return (
         <Section className={'py-0 pt-16'}>
          <div className='grid lg:grid-cols-[2fr,1fr] md:grid-cols-[2fr,1fr] gap-5'>
-                <div className='lg:w-full lg:h-full h-[50vh] lg:flex hidden'>
+                <div className='lg:w-full lg:h-full h-[50vh] lg:flex hidden bg-[url(`${Src_two}`)]'>
                     <iframe width="100%" height="100%" src="https://www.youtube.com/embed/mV8MWjhvZvk?si=xzl53vKNad8M--KI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 </div>
                 <div>
